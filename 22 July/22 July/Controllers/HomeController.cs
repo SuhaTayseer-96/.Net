34fc82573971b435a96c27dc6e5a07fd7185599a
SuhaKrasneh <suha.kras967@gmail.com>
2024-07-22 18:04:03 +0300

    .net

---
+++ 22 July/22 July/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _22_July.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Recipes()
+        {
+            ViewBag.Message = "Your Recipes description page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your About page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your Contact page.";
+
+            return View();
+        }
+        public ActionResult Donate()
+        {
+            ViewBag.Message = "Your Donate page.";
+
+            return View();
+        }
+    }
+}
