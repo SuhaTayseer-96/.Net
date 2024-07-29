69922deaf14249b2b99b0daadde3eb7b0ed070c4
SuhaKrasneh <suha.kras967@gmail.com>
2024-07-30 00:37:18 +0300

    update

---
+++ 29 July - Task 6/29 July - Task 6/Controllers/ORCo4Controller.cs
@@ -0,0 +1,66 @@
+﻿using _29_July___Task_6.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _29_July___Task_6.Controllers
+{
+    public class ORCo4Controller : Controller
+    {
+        private users_CRUDEntities db = new users_CRUDEntities();
+        // GET: ORCo4
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(users_CRUD User_, string confirmpassword)
+        {
+            if (User_.Password == confirmpassword) { 
+            db.users_CRUD.Add(User_);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Login(string inputEmail, string inputPassword)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(users_CRUD User_) {
+
+
+            var checkInput = db.users_CRUD.Where(ModelBinderAttribute => ModelBinderAttribute.Email && model.Password == User_.Password.FirstOrDefault
+            );
+            Session["ID"] = checkInput.ID;
+            if (checkInput != null)
+            { 
+            return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            var ID =(int)Session["ID"];
+            var user = db.users.Find(userID);
+            return View(db.users);
+        }
+        [HttpPost]
+        public ActionResult Profile(users_CRUD User_)
+        {
+           db.Entry(User_).State = EntityState.Modified;
+            db.SaveChanges();
+            return View(db.users);
+        }
+
+    }
+}
---
+++ 29 July - Task 6/29 July - Task 6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_29_July___Task_6.MvcApplication" Language="C#" %>
